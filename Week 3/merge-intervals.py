d476cdeb01cda3119cca702fa4fe0152fbdf0d91
ruthgdb <ruthgd2000@gmail,com>
2022-01-02 02:50:58 +0300

    Week 3 update

--- Week 3/merge-intervals.py
+++ Week 3/merge-intervals.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
@@ -13,20 +12,5 @@ class Solution:
                 start = intervals[i][0]
                 end = intervals[i][1]
         newlist.append([start, end])
-=======
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()
-        start = intervals[0][0]
-        end = intervals[0][1]
-        newlist = list()
-        for i in range(1, len(intervals)):
-            if intervals[i][0] <= end:
-                end = max(intervals[i][1], end)
-            else:
-                newlist.append([start,end])
-                start = intervals[i][0]
-                end = intervals[i][1]
-        newlist.append([start, end])
->>>>>>> eb99cca94aa3d6f79b9499f04a185a0ffd2328cb
         return newlist
+        
