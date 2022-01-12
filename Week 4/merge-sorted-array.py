ae830f00f9532fcd3eeb450841e78ca35ddcac9b
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-01-12 03:18:35 +0300

    Update merge-sorted-array.py

--- Week 4/merge-sorted-array.py
+++ Week 4/merge-sorted-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        x = 0
+        nums2_index = 0
         for i in range(m,n+m):
-            nums1[i] = nums2[x]
-            x += 1
+            nums1[i] = nums2[nums2_index]
+            nums2_index += 1
         nums1.sort()
