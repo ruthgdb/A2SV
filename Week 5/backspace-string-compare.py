812651b9cccf41aa80b07d8184af72d7575efa07
ruthgdb <ruthgd2000@gmail,com>
2022-01-21 19:14:14 +0300

    Week 5 commit

---
+++ Week 5/backspace-string-compare.py
@@ -0,0 +1,23 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1 = []
+        stack2 = []
+        
+        for element in s:
+            if element == '#':
+                if stack1:
+                    stack1.pop()
+            else:
+                stack1.append(element)
+                
+        for element in t:
+            if element == '#':
+                if stack2:
+                    stack2.pop()
+            else:
+                stack2.append(element)
+        
+        if stack1 == stack2:
+            return True 
+        else:
+            return False
