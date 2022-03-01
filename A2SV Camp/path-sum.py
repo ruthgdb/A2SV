ec492cb215fd077b88523bad5ca958065fa26c71
ruthgdb <ruthgd2000@gmail,com>
2022-03-01 09:29:27 +0300

    Camp commits

---
+++ A2SV Camp/path-sum.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        if not root.right and not root.left:
+            if targetSum == root.val:
+                return True
+            else:
+                return False
+        
+        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
