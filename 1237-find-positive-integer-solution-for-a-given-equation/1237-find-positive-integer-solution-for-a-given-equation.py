ba503d0320572edd61264b1418d663c87fe2cf9a
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-03-17 10:56:31 +0300

    Time: 389 ms (6.24%), Space: 14 MB (26.81%) - LeetHub

---
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.py
@@ -0,0 +1,31 @@
+"""
+   This is the custom function interface.
+   You should not implement it, or speculate about its implementation
+   class CustomFunction:
+       # Returns f(x, y) for any given positive integers x and y.
+       # Note that f(x, y) is increasing with respect to both x and y.
+       # i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+       def f(self, x, y):
+  
+"""
+
+class Solution:
+    def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+        res = []
+        
+        for x in range(1, 1001):
+            left = 1
+            right = 1000
+            
+            while left <= right:
+                mid = left + (right - left) // 2
+                
+                if customfunction.f(x, mid) == z:
+                    res.append([x, mid])
+                    break
+                elif customfunction.f(x, mid) > z:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        
+        return res
