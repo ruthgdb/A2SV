c106f4a73e380a7a0b0f4a595a7f4c1f23e45c0e
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-04-07 02:04:10 +0300

    Time: 298 ms (16.58%), Space: 14.4 MB (33.46%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -1,27 +1,24 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         count = 0
+        length = len(flowerbed)
         
-        if len(flowerbed) == 1:
-            if flowerbed[0] == 1 and n == 0:
-                return True
-            elif flowerbed[0] == 1 and n > 0:
-                return False
-            elif flowerbed[0] == 0 and n > 1:
-                return False
-            return True
-        
-        if flowerbed[0] == 0 and flowerbed[1] == 0:
-            count += 1
-            flowerbed[0] = 1
-        if flowerbed[len(flowerbed) - 1] == 0 and flowerbed[len(flowerbed) - 2] == 0:
-            count += 1
-            flowerbed[len(flowerbed) - 1] = 1
-        
-        for i in range(1, len(flowerbed) - 1):
-            if flowerbed[i + 1] == flowerbed[i - 1] == flowerbed[i]:
+        if length == 1:
+            if flowerbed[0] == 0:
                 count += 1
-                flowerbed[i] = abs(flowerbed[i] - 1)
+                n -= 1
+        else:
+            if flowerbed[0] == 0 and flowerbed[1] == 0:
+                count += 1
+                flowerbed[0] = 1
+            if flowerbed[length - 1] == 0 and flowerbed[length - 2] == 0:
+                count += 1
+                flowerbed[length - 1] = 1
+
+            for i in range(1, length - 1):
+                if flowerbed[i + 1] == flowerbed[i - 1] == flowerbed[i]:
+                    count += 1
+                    flowerbed[i] = abs(flowerbed[i] - 1)
 
         if n <= count:
             return True
