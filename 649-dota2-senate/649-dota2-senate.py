f4379d29e49fae699ded34c169f2399456ab87d1
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-04-08 00:21:32 +0300

    Time: 704 ms (13.06%), Space: 14.1 MB (79.73%) - LeetHub

---
+++ 649-dota2-senate/649-dota2-senate.py
@@ -0,0 +1,19 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        queue = deque(senate)
+        count = Counter(queue)
+                
+        while queue or count['R'] != 0 or count['D'] != 0:
+            temp = queue.popleft()
+            if temp == 'D' and 'R' in queue:
+                queue.remove('R')
+                count['R'] -= 1
+            elif temp == 'D' and 'R' not in queue:
+                return "Dire"
+            elif temp == 'R' and 'D' in queue:
+                queue.remove('D')
+                count['D'] -= 1
+            else:
+                return "Radiant"
+            queue.append(temp)
+            
