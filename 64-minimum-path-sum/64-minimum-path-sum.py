f7c0107fe16f54bb6dc8fe381efe4921b1278809
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-04-09 12:09:00 +0300

    Time: 208 ms (12.91%), Space: 19.8 MB (12.26%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,30 +1,21 @@
+from collections import defaultdict
+
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        memo = dict()
+        memo = defaultdict(int)
         n, m = len(grid), len(grid[0])
         
         def dfs(i, j):
-            if (i, j) in memo.keys():
-                return memo[(i, j)]
+            if i >= n or j >= m:
+                return float("inf")
             
             if i == n - 1 and j == m - 1:
-                memo[(i, j)] = grid[i][j]
-                return memo[(i, j)]
-            if i == n - 1:
-                memo[(i, j)] = dfs(i, j + 1) + grid[i][j]
-                return memo[(i, j)]
-            if j == m - 1:
-                memo[(i, j)] = dfs(i + 1, j) + grid[i][j]
+                return grid[i][j]
+            
+            if (i, j) in memo: 
                 return memo[(i, j)]
             
             memo[(i, j)] = min(dfs(i + 1, j), dfs(i, j + 1)) + grid[i][j]
             return memo[(i, j)]
-        
-        if n == m == 1:
-            return grid[0][0]
-        if n == 1:
-            return grid[0][0] + dfs(0, 1)
-        if m == 1:
-            return grid[0][0] + dfs(1, 0)
-                    
-        return grid[0][0] + min(dfs(0, 1), dfs(1, 0))
+                
+        return dfs(0, 0)
