3fe5a076b4cd9ef2cf7e53dcf3567a51fedbf9aa
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-05-02 20:29:22 +0300

    Time: 45 ms (47.85%), Space: 14.1 MB (6.90%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,23 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        
-        if n == 1:
+        if len(nums) == 1:
             return nums[0]
         
-        maximum = 0
-        withoutLast, withoutFirst = [0] * n, [0] * n
+        memo1 = {}
+        memo2 = {}
         
-        for i in range(n - 2, -1, -1):
-            house1 = withoutFirst[i + 2] if i + 2 < n else 0
-            house2 = withoutFirst[i + 3] if i + 3 < n else 0
-            withoutFirst[i] = max(house1, house2) + nums[i]
-            maximum = max(maximum, withoutFirst[i])
+        def withoutFirst(i):
+            if i >= len(nums):
+                return 0
             
-        for i in range(n - 1, 0, -1):
-            house1 = withoutLast[i + 2] if i + 2 < n else 0
-            house2 = withoutLast[i + 3] if i + 3 < n else 0
-            withoutLast[i] = max(house1, house2) + nums[i]
-            maximum = max(maximum, withoutLast[i])
+            if i not in memo1:
+                memo1[i] = max(withoutFirst(i + 1), withoutFirst(i + 2) + nums[i])
+            
+            return memo1[i]
         
-        return maximum
+        def withoutLast(i):    
+            if i >= len(nums) - 1:
+                return 0
+            
+            if i not in memo2:
+                memo2[i] = max(withoutLast(i + 1), withoutLast(i + 2) + nums[i])
+            
+            return memo2[i]         
+        
+        
+        return max(withoutFirst(1), withoutLast(0))
