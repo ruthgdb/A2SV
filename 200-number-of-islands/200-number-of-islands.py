8cb575d13240b5e763e96bba575acc6e8c230675
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-07-20 23:34:24 +0300

    Time: 471 ms (51.98%), Space: 25.1 MB (10.79%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,26 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
-        inbound = lambda r, c: 0 <= r < len(grid) and 0 <= c < len(grid[0])
-        visited = set()
         count = 0
+        n, m = len(grid), len(grid[0])
+        inbound = lambda r, c: 0 <= r < n and 0 <= c < m
+        DIRs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        visited = set()
         
-        def dfs(row, col):
-            visited.add((row,col))
+        def dfs(r, c):
+            visited.add((r, c))
             
-            for direction in directions:
-                newRow = row + direction[0]
-                newCol = col + direction[1]
-                if inbound(newRow, newCol) and \
-            grid[newRow][newCol] == "1" and (newRow, newCol) not in visited:
-                    dfs(newRow, newCol)
+            for DIR in DIRs:
+                nr = r + DIR[0]
+                nc = c + DIR[1]
+                
+                if inbound(nr, nc) and grid[nr][nc] == '1' and (nr, nc) not in visited:
+                    dfs(nr, nc)
             
             
-        start = grid[0][0]
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if (row,col) not in visited and grid[row][col] == "1":
+        for i in range(n):
+            for j in range(m):
+                if (i, j) not in visited and grid[i][j] == '1':
+                    dfs(i, j)
                     count += 1
-                    dfs(row, col)
-        
+                    
         return count
+        
