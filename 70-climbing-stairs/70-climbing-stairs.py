318ac8578d740a8151532970d3afd45c258257f3
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-07-21 20:28:41 +0300

    Time: 65 ms (7.71%), Space: 13.9 MB (56.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,8 +1,12 @@
 class Solution:
-    def climbStairs(self, n: int, memo = {1: 1, 2: 2}) -> int:
-        if n not in memo.keys():
-            memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-
-        return memo[n]
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
         
+        dp = [0] * n
+        dp[0], dp[1] = 1, 2
         
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+            
+        return dp[-1]
