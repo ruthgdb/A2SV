2a502245294bdb8cb06dab22adaab0fa6b666f66
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-07-28 17:22:39 +0300

    Time: 57 ms (81.72%), Space: 14.3 MB (96.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,13 +1,4 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        freqS = Counter(s)
-        freqT = Counter(t)
-        
-        for i in t:
-            if i not in freqS or freqS[i] != freqT[i]:
-                return False
-            
-        return True
+    def isAnagram(self, s: str, t: str) -> bool: 
+        freqS, freqT = Counter(s), Counter(t)
+        return freqS == freqT
