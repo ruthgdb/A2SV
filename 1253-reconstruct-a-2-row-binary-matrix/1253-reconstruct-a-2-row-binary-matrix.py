5212299f40f2bc13c2b624ad08958f2802ad4269
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-08-08 22:24:30 +0300

    Time: 1398 ms (8.67%), Space: 24.6 MB (18.50%) - LeetHub

--- 1253-reconstruct-a-2-row-binary-matrix/1253-reconstruct-a-2-row-binary-matrix.py
+++ 1253-reconstruct-a-2-row-binary-matrix/1253-reconstruct-a-2-row-binary-matrix.py
@@ -3,25 +3,21 @@ class Solution:
         if upper + lower != sum(colsum):
             return []
         
-        matrix, n, u, l = [], len(colsum), upper, lower
-        
-        for _ in range(2):
-            temp = [0 for x in range(n)]
-            matrix.append(temp)
-            
+        n = len(colsum)
+        matrix = [[0 for x in range(n)],  [0 for x in range(n)]]
+
         for i in range(n):
             if colsum[i] == 2:
                 matrix[0][i], matrix[1][i] = 1, 1
-                u -= 1
-                l -= 1
+                upper, lower = upper - 1, lower - 1
                 
         for i in range(n):
             if colsum[i] == 1:
-                if u > 0:
+                if upper > 0:
                     matrix[0][i] = 1 
-                    u -= 1
+                    upper -= 1
                 else:
                     matrix[1][i] = 1 
-                    l -= 1
+                    lower -= 1
         
-        return [] if sum(matrix[0]) != upper or sum(matrix[1]) != lower else matrix
+        return [] if upper != 0 or lower != 0 else matrix
