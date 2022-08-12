05a6c32df5c5298c506cf6d4d8afdcfd1f88928e
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-08-12 15:41:07 +0300

    Time: 56 ms (23.95%), Space: 13.8 MB (71.86%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,18 +1,25 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack=[]
-        
-        for i in range(len(s)):
-            if s[i]!=']':
+        stack, i, res = [s[0]], 1, ''
+
+        while i < len(s):
+            if s[i] != ']':
                 stack.append(s[i])
             else:
-                substr=''
-                while stack[-1]!='[':
-                    substr=stack.pop()+substr
+                temp, num = '', ''
+                
+                while stack[-1] != '[':
+                    temp = stack.pop() + temp                  
                 stack.pop()
-                k=''
+                
                 while stack and stack[-1].isdigit():
-                    k=stack.pop()+k
-
-                stack.append(int(k)*substr)
-        return ''.join(stack)
+                    num = stack.pop() + num        
+                stack.append(temp * int(num))
+                
+            i += 1
+            
+        for i in stack:
+            res += i
+            
+        return res
+   
