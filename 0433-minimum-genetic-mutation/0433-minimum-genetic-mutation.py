d195ac8f97c3444aa5fac9875c54e8ebb0350066
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-11-02 07:31:44 +0300

    Time: 61 ms (31.51%), Space: 13.8 MB (86.98%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        if end not in bank: return -1
+        
+        bank.append(start)
+        visited = set(start)
+        queue = deque([start])
+        max_len = 0
+        mutations = defaultdict(list)
+        
+        for gene in bank:
+            for i in range(8):
+                changes = gene[:i] + '*' + gene[i + 1:]
+                mutations[changes].append(gene)
+                                    
+        while queue:
+            l = len(queue)
+            
+            for i in range(l):
+                curr_mutation = queue.popleft()
+                if curr_mutation == end:
+                    return max_len
+                
+                for i in range(len(curr_mutation)):
+                    changes = curr_mutation[:i] + '*' + curr_mutation[i + 1:]
+                    
+                    for match in mutations[changes]:
+                        if match not in visited:
+                            queue.append(match)
+                            visited.add(match)
+            max_len += 1
+                            
+        return -1
