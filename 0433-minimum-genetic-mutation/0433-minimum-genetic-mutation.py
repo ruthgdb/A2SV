4c9bb6ec1953efd00a42ab18152232002283f42c
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-11-02 08:19:36 +0300

    Time: 62 ms (28.70%), Space: 13.9 MB (37.43%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,19 +1,19 @@
 class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        if end not in bank: 
+        if end not in bank:
             return -1
         
-        bank.append(start)
         visited = set(start)
-        queue = deque([start])
         min_len = 0
-        mutations = defaultdict(list)
+        changes = defaultdict(list)
+        queue = deque([start])
+        bank.append(start)
         
-        for gene in bank:
+        for change in bank:
             for i in range(8):
-                changes = gene[:i] + '*' + gene[i + 1:]
-                mutations[changes].append(gene)
-                                    
+                mutation = change[:i] + '*' + change[i + 1:]
+                changes[mutation].append(change)
+                
         while queue:
             l = len(queue)
             
@@ -21,14 +21,15 @@ class Solution:
                 curr_mutation = queue.popleft()
                 if curr_mutation == end:
                     return min_len
-                
+
                 for i in range(len(curr_mutation)):
-                    changes = curr_mutation[:i] + '*' + curr_mutation[i + 1:]
+                    mutation = curr_mutation[:i] + '*' + curr_mutation[i + 1:]
                     
-                    for match in mutations[changes]:
-                        if match not in visited:
-                            queue.append(match)
-                            visited.add(match)
+                    for change in changes[mutation]:
+                        if change not in visited:
+                            visited.add(change)
+                            queue.append(change)
+                        
             min_len += 1
-                            
+            
         return -1
