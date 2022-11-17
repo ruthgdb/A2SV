342c554d168af6062b650d896db11951777b2c7e
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-11-17 18:22:30 +0300

    Time: 4406 ms (21.51%), Space: 44.5 MB (5.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,19 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:        
-        @lru_cache(None)
+        memo = {}
+        
         def traverse(total):
-            if total == amount:
+            if total in memo:
+                return memo[total]
+            
+            if total == 0:
                 return 0
             
-            if total > amount:
+            if total < 0:
                 return float("inf")
                 
             mini = float("inf")
             
             for coin in coins:
-                mini = min(mini, traverse(total + coin))
+                mini = min(mini, traverse(total - coin) + 1)
             
-            return mini + 1
+            memo[total] = mini
+            return mini
             
-        min_coins = traverse(0)
+        min_coins = traverse(amount)
         return min_coins if min_coins != float("inf") else -1
