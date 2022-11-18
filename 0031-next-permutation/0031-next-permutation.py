7faa3d4f7601606e64c29d58ba12bb098a2d4e8f
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-11-18 17:42:25 +0300

    Time: 71 ms (71.86%), Space: 13.8 MB (75.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -8,24 +8,23 @@ class Solution:
         for i in range(len(nums) - 2, -1, -1):
             if nums[i] < nums[i + 1]:
                 smaller_idx = i
+                smaller_num, idx = float("inf"), -1
+
+                for i in range(smaller_idx + 1, len(nums)):
+                    if nums[i] > nums[smaller_idx] and nums[i] <= smaller_num:
+                        smaller_num = nums[i]
+                        idx = i
+
+                nums[idx], nums[smaller_idx] = nums[smaller_idx], nums[idx]
+
+                left, right = smaller_idx + 1, len(nums) - 1
+
+                while left < right:
+                    if nums[left] > nums[right]:
+                        nums[left], nums[right] = nums[right], nums[left]
+                    left += 1
+                    right -= 1
                 break
-                   
+        
         if smaller_idx == -1:
             nums.sort()
-        else:
-            smaller_num, idx = float("inf"), -1
-
-            for i in range(smaller_idx + 1, len(nums)):
-                if nums[i] > nums[smaller_idx] and nums[i] <= smaller_num:
-                    smaller_num = nums[i]
-                    idx = i
-            
-            nums[idx], nums[smaller_idx] = nums[smaller_idx], nums[idx]
-            
-            left, right = smaller_idx + 1, len(nums) - 1
-            
-            while left < right:
-                if nums[left] > nums[right]:
-                    nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-                right -= 1
