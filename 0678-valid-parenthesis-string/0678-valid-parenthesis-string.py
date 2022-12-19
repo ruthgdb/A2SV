88b055cdcd58a007a4cd7b5c31f548649d1bbd88
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-12-19 11:17:21 +0300

    Time: 38 ms (80.26%), Space: 13.9 MB (71.18%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,25 +1,33 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
+        opens = 0
+        close = 0
+        asterisk = 0
         
-        @cache
-        def dp(i, count):
-            if i == len(s):
-                return count == 0
-            
-            if count < 0:
+        for i in s:
+            if i == '(':
+                opens += 1
+            if i == ')':
+                close += 1
+            if i == '*':
+                asterisk += 1
+                
+            if close > asterisk + opens:
                 return False
             
-            ifOpen = ifClose = ifEmpty = False
-            
-            if s[i] == '*':
-                ifOpen = dp(i + 1, count + 1)
-                ifClose = dp(i + 1, count - 1)
-                ifEmpty = dp(i + 1, count)
-            elif s[i] == '(':
-                ifOpen = dp(i + 1, count + 1)
-            else:
-                ifClose = dp(i + 1, count - 1)
+        opens = 0
+        close = 0
+        asterisk = 0
+        
+        for i in reversed(s):
+            if i == '(':
+                opens += 1
+            if i == ')':
+                close += 1
+            if i == '*':
+                asterisk += 1
                 
-            return ifOpen or ifClose or ifEmpty
+            if opens > asterisk + close:
+                return False
             
-        return dp(0, 0)
+        return True
