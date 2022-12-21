495b3a8855c6437ac1269db74a008c5477c35733
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-12-21 11:56:13 +0300

    Time: 1081 ms (25.93%), Space: 34.2 MB (6.91%) - LeetHub

--- 0975-odd-even-jump/0975-odd-even-jump.py
+++ 0975-odd-even-jump/0975-odd-even-jump.py
@@ -1,60 +1,48 @@
 class Solution:
-    def buildStack(self, arr, jumps):
-        ordered = [(value, idx) for idx, value in enumerate(arr)]
-        ordered.sort()
+    def buildStack(self, jumps, ordered, parity):
+        if parity:
+            ordered.sort(reverse = True)
+        else:
+            ordered.sort(key = lambda x: (x[0], -x[1]))
+            
         stack = []
-        # print(ordered)
-
-        #for odd
-        for i in range(len(arr) - 1, -1, -1):
+        
+        for i in range(len(ordered)):
+            idx = 0 if parity else 1
+            
             while stack and stack[-1] < ordered[i][1]:
                 stack.pop()
                 
             if stack:
-                jumps[ordered[i][1]][0] = stack[-1]
+                jumps[ordered[i][1]][idx] = stack[-1]
             
             stack.append(ordered[i][1])
 
-        stack = []
-        ordered.sort(key = lambda x: (x[0], -x[1]))
-        
-        #for even
-        for i in range(len(arr)):
-            while stack and stack[-1] < ordered[i][1]:
-                stack.pop()
-                
-            if stack:
-                jumps[ordered[i][1]][1] = stack[-1]
-            
-            stack.append(ordered[i][1])
-        
     def oddEvenJumps(self, arr: List[int]) -> int:
         count = 0
-        # odd, even
         jumps = defaultdict(lambda: [-1, -1])
-        self.buildStack(arr, jumps)
+        ordered = [(value, idx) for idx, value in enumerate(arr)]
+        self.buildStack(jumps, ordered, True)
+        self.buildStack(jumps, ordered, False)
 
+        @cache
         def dp(i, parity):
             if i == len(arr) - 1:
                 return True
             
-            # print(i, parity)
             if parity:
                 nextJump = jumps[i][0]
                 if nextJump != -1:
                     return dp(nextJump, 0)
-                else:
-                    return False
+                return False
             else:
                 nextJump = jumps[i][1]
                 if nextJump != -1:
                     return dp(nextJump, 1)
-                else:
-                    return False
+                return False
         
         for i in range(len(arr)):
             if dp(i, 1):
-                # print(i)
                 count += 1
                 
         return count
