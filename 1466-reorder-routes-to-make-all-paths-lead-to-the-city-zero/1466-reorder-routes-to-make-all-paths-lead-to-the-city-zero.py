99819aea5c1a7cb1be7ce15c10feaf296b67e9a3
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-12-24 22:11:17 +0300

    Time: 1148 ms (99.24%), Space: 41.7 MB (87.36%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        queue = deque([0])
+        visited = set()
+        changedEdges = 0
+        
+        for city1, city2 in connections:
+            graph[city1].append((city2, "from"))
+            graph[city2].append((city1, "to"))
+        
+        while queue:
+            currCity = queue.popleft()
+
+            if currCity in visited:
+                continue
+                
+            visited.add(currCity)
+            
+            for neighbour, d in graph[currCity]:
+                if neighbour not in visited:
+                    if d == 'from':
+                        changedEdges += 1
+                    queue.append(neighbour)
+                             
+        return changedEdges
