61a6018efa027f64d447c0bee4a92252e7249d21
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2022-12-25 14:17:40 +0300

    Time: 1465 ms (66.48%), Space: 47 MB (5.04%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,49 +1,57 @@
 class Solution:
-    def buildRanges(self, ranges, heights):
+    def buildRangeFromLeft(self, ranges, heights):
         stack = []
         
         for i, height in enumerate(heights):            
-            if stack and stack[-1][0] > height:
-                currMax, maxIdx = stack.pop()
-                ranges[maxIdx] = 0
-                
-                while stack and stack[-1][0] > height:
-                    temp, idx = stack.pop()
-                    ranges[idx] += maxIdx - idx
+            currMax = None
+
+            while stack and stack[-1][0] > height:
+                temp, idx = stack.pop()
+                if not currMax:
+                    currMax, maxIdx = temp, idx
+                    ranges[maxIdx] = 0
+                ranges[idx] += maxIdx - idx
+                    
             stack.append((height, i))
             
-        if stack:
-            currMax, maxIdx = stack.pop()
-            ranges[maxIdx] = 0
-            
-            while stack:
-                temp, idx = stack.pop()
-                ranges[idx] += maxIdx - idx
-                
+        currMax = None
+
+        while stack:
+            temp, idx = stack.pop()
+            if not currMax:
+                currMax, maxIdx = temp, idx
+                ranges[maxIdx] = 0
+
+            ranges[idx] += maxIdx - idx
+             
+    def buildRangeFromRight(self, ranges, heights):
         stack = []
 
         for i in range(len(heights) - 1, -1, -1):            
-            if stack and stack[-1][0] > heights[i]:
-                currMax, maxIdx = stack.pop()
-                
-                while stack and stack[-1][0] > heights[i]:
-                    temp, idx = stack.pop()
-                    ranges[idx] += abs(maxIdx - idx)
-            
+            currMax = None
+
+            while stack and stack[-1][0] > heights[i]:
+                temp, idx = stack.pop()
+                if not currMax:
+                    currMax, maxIdx = temp, idx
+                ranges[idx] += abs(maxIdx - idx)
+
             stack.append((heights[i], i))
             
-        if stack:
-            currMax, maxIdx = stack.pop()
+        currMax = None
 
-            while stack:
-                temp, idx = stack.pop()
-                ranges[idx] += abs(maxIdx - idx)
+        while stack:
+            temp, idx = stack.pop()
+            if not currMax:
+                currMax, maxIdx = temp, idx
+            ranges[idx] += abs(maxIdx - idx)
         
                 
     def largestRectangleArea(self, heights: List[int]) -> int:
         ranges = defaultdict(int)
         largestRectangle = 0
-        self.buildRanges(ranges, heights)
+        self.buildRangeFromLeft(ranges, heights)
+        self.buildRangeFromRight(ranges, heights)
 
         for i in ranges:
             largestRectangle = max(largestRectangle, (ranges[i] + 1) * heights[i])
