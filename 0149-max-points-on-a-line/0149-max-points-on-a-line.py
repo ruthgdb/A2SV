4b7a82f238c209c9d6dc152a58bdaf897f4d3108
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-01-08 16:32:39 +0300

    Time: 154 ms (62.52%), Space: 14 MB (53.74%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,10 +1,4 @@
 class Solution:
-    def calcSlope(self, x1, y1, x2, y2):
-        if x2 == x1:
-            return float("inf")
-        
-        return (y2 - y1) / (x2 - x1)
-        
     def maxPoints(self, points: List[List[int]]) -> int:
         slopes = defaultdict(set)
         sameLine = 1
@@ -18,14 +12,13 @@ class Solution:
                 if i == j:
                     continue
                     
-                slope = self.calcSlope(points[i][0], points[i][1], points[j][0], points[j][1])
-                
-                if slope == float("inf"):
+                if points[j][0] == points[i][0]:
                     sameLine += 1
+                    maxPoints = max(maxPoints, sameLine)
                 else:
+                    slope = (points[j][1] - points[i][1]) / (points[j][0] - points[i][0])
                     slopes[slope].add(tuple(points[i]))
                     slopes[slope].add(tuple(points[j]))
-        
-                maxPoints = max(maxPoints, len(slopes[slope]), sameLine)
+                    maxPoints = max(maxPoints, len(slopes[slope]))
 
         return maxPoints
