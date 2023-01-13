5ff2c42fcd1cc99e6208884a959f44376ab5a2d9
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-01-13 22:11:16 +0300

    Time: 1731 ms (93.02%), Space: 58.8 MB (89.57%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,29 @@
+class Solution:
+    def find(self, node, parents):
+        if parents[node] == node:
+            return node
+        
+        parents[node] = self.find(parents[node], parents)
+        return parents[node]
+        
+    def union(self, u, v, dist, parents, rank):
+        pu = self.find(u, parents)
+        pv = self.find(v, parents)
+        
+        if pu != pv:
+            parents[pu] = pv
+            
+        rank[pv] = min(rank[pv], rank[pu], dist)
+        rank[pu] = min(rank[pv], rank[pu], dist)
+        
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        rank = [float("inf")] * n
+        parents = [i for i in range(n)]
+        
+        for city1, city2, dist in roads:
+            self.union(city1 - 1, city2 - 1, dist, parents, rank)
+           
+        for i in range(n):
+            self.find(i, parents)
+        
+        return rank[self.find(0, parents)]
