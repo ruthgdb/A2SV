964d647a588be57b48050719a46d3ee05513eb1c
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-01-19 12:58:52 +0300

    Time: 1875 ms (38.14%), Space: 14.2 MB (47.41%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.py
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.py
@@ -1,28 +1,15 @@
 class Solution:
-    def checkRectangle(self, x1, x2, pos):
-        minArea = float("inf")
-        
-        for y1 in pos[x1]:
-            if y1 in pos[x2]:
-                for y2 in pos[x2]:
-                    if y1 != y2 and y2 in pos[x1]:
-                        minArea = min(minArea, abs(x1 - x2) * abs(y2 - y1))
-                     
-        return minArea
-        
     def minAreaRect(self, points: List[List[int]]) -> int:
         points.sort()
-        pos = defaultdict(set)
+        pointsSet = set([(x, y) for x, y in points])
         area = float("inf")
-        visited = set()
         
-        for x, y in points:
-            pos[x].add(y)
-            
-        for pt1 in pos:
-            for pt2 in pos:
-                if pt1 != pt2 and (pt1, pt2) not in visited:
-                    visited.add((pt2, pt1))
-                    area = min(area, self.checkRectangle(pt1, pt2, pos))
-            
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                
+                if x1 != x2 and y1 != y2 and (x1, y2) in pointsSet and (x2, y1) in pointsSet:
+                    area = min(area, abs(x1 - x2) * abs(y1 - y2))
+        
         return area if area != float("inf") else 0
