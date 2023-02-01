4f17b33f5737180d77cded3c0cba2a1912768618
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-02-01 15:45:59 +0300

    Time: 33 ms (78.11%), Space: 13.9 MB (61.37%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,22 +1,19 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
         wCount = 0
-        bCount = 0
         l = 0
         minFlips = k
         
         for r in range(len(blocks)):
             if blocks[r] == 'W':
                 wCount += 1
-            else:
-                bCount += 1
                 
             if r - l + 1 > k:
                 if blocks[l] == 'W':
                     wCount -= 1
-                else:
-                    bCount -= 1
+                
                 l += 1
+                
             if r - l + 1 == k:
                 minFlips = min(minFlips, wCount)
                 
