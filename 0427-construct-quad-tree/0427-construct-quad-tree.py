670b8d982a90ff9176cf914a8c96b7aeb7d39013
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-02-27 17:14:45 +0300

    Time: 112 ms (81.82%), Space: 14.8 MB (41.32%) - LeetHub

---
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -0,0 +1,42 @@
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        root = Node()
+        n = len(grid)
+         
+        def checkSubMatrix(r1, c1, n):
+            total = 0
+            
+            for r in range(r1, r1 + n):
+                total += sum(grid[r][c1:c1 + n])
+                
+            return total == 0 or total == n ** 2
+        
+        def dfs(r1, c1, n, root):
+            leaf = checkSubMatrix(r1, c1, n)
+            
+            if leaf:
+                root.isLeaf = True
+                root.val = grid[r1][c1]
+            else:
+                root.isLeaf = False
+                root.val = grid[r1][c1]
+                root.topLeft = dfs(r1, c1, n // 2, Node())
+                root.topRight = dfs(r1, c1 + (n // 2), n // 2, Node())
+                root.bottomLeft = dfs(r1 + (n // 2), c1, n // 2, Node())
+                root.bottomRight = dfs(r1 + (n // 2), c1 + (n // 2), n // 2, Node())
+            
+            return root
+        
+        return dfs(0, 0, n, root)
