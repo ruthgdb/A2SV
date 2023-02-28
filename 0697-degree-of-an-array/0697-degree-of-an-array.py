e4d899e42e5ba400d92bdbd193a1d6bea14aac29
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-02-28 10:16:39 +0300

    Time: 278 ms (37.47%), Space: 15.6 MB (30.07%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -1,17 +1,18 @@
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        indices = defaultdict(list)
+        indices = defaultdict(lambda: [float("inf"), float("-inf")])
         count = Counter(nums)
         maxCount = max(count.values())
         minLength = float("inf")
         
         for i, num in enumerate(nums):
-            indices[num].append(i)
-            
+            indices[num][0] = min(indices[num][0], i)
+            indices[num][1] = max(indices[num][1], i)
+                        
         for num in count:
             if count[num] == maxCount:
                 first = indices[num][0]
-                last = indices[num][-1]
+                last = indices[num][1]
                 minLength = min(minLength, last - first + 1)
                 
         return minLength
