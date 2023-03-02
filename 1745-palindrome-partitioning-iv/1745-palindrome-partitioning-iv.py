f9daeaf73946c1a4c62a336cc37dbacfcd54c6eb
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-03-02 23:30:18 +0300

    Time: 1389 ms (97.33%), Space: 63.1 MB (29.33%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkPartitioning(self, s: str) -> bool:
+        n = len(s)
+        palindroms = defaultdict(list)
+        dp = [[False, False, False, False] for _ in range(n + 1)]
+        dp[0][0] = True
+        
+        for i in range(n):
+            left, right = i, i
+            
+            while left >= 0 and right < n and s[left] == s[right]:
+                palindroms[left].append(right + 1)
+                left -= 1
+                right += 1
+                    
+            left, right = i, i + 1
+            
+            while left >= 0 and right < n and s[left] == s[right]:
+                palindroms[left].append(right + 1)
+                left -= 1
+                right += 1
+         
+        for i in range(n):
+            for j in palindroms[i]:
+                dp[j][1] = dp[j][1] or dp[i][0] 
+                dp[j][2] = dp[j][2] or dp[i][1]
+                dp[j][3] = dp[j][3] or dp[i][2]
+                
+        return dp[-1][3]
