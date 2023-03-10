9d834967562c122e42e47c81aa3b7aef9b78f415
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-03-10 18:38:07 +0300

    Time: 1030 ms (61.47%), Space: 36.2 MB (41.27%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -1,42 +1,20 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-        paper = 0
-        pt = 0
         
-        for i in range(len(garbage)):
-            count = garbage[i].count('P')
-            
-            if i > 0:
-                pt += travel[i - 1]
-            
-            if count > 0:
-                paper += count + pt
-                pt = 0
-            
-        glass = 0
-        gt = 0
+        def calculateTime(gType):
+            dist = 0
+            t = 0
+
+            for i in range(len(garbage)):
+                count = garbage[i].count(gType)
+
+                if i > 0:
+                    t += travel[i - 1]
+
+                if count > 0:
+                    dist += count + t
+                    t = 0
+                    
+            return dist
         
-        for i in range(len(garbage)):
-            count = garbage[i].count('G')
-            
-            if i > 0:
-                gt += travel[i - 1]
-            
-            if count > 0:
-                glass += count + gt
-                gt = 0
-      
-        metal = 0
-        mt = 0
-        
-        for i in range(len(garbage)):
-            count = garbage[i].count('M')
-            
-            if i > 0:
-                mt += travel[i - 1]
-            
-            if count > 0:
-                metal += count + mt
-                mt = 0
-        
-        return paper + metal + glass
+        return calculateTime('P') + calculateTime('G') + calculateTime('M')
