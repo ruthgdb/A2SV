b13f28f916868959a321c41c39704c8020117a13
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-03-10 19:28:15 +0300

    Time: 107 ms (82.85%), Space: 14.1 MB (98.59%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -2,13 +2,13 @@ class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         nums.sort()
         prefSum = [nums[0]]
-        answer = []
+        res = []
         
         for i in range(1, len(nums)):
             prefSum.append(prefSum[-1] + nums[i])
-        
-        for i in range(len(queries)):
-            idx = bisect.bisect(prefSum, queries[i])
-            answer.append(idx)
             
-        return answer
+        for query in queries:
+            i = bisect.bisect_right(prefSum, query)
+            res.append(i)
+            
+        return res
