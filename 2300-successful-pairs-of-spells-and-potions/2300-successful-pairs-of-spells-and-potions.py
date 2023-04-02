a39005d96f15aebe2b9605c36e8ba91c8f83273d
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-04-02 09:26:10 +0300

    Time: 1213 ms (92.92%), Space: 37.4 MB (27.20%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,4 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        return [len(potions) - bisect.bisect_left(potions, math.ceil(success/spell)) for i, spell in enumerate(spells)]
