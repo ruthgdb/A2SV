0aa32b21303fd8d912dfc0a3aa412028598c6865
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-04-16 21:39:54 +0300

    Time: 2709 ms (37.74%), Space: 269.9 MB (25.68%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -1,11 +1,11 @@
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
         mod = 10 ** 9 + 7
-        chars = defaultdict(int)
+        chars = [defaultdict(int) for _ in range(len(words[0]))]
         
-        for word in words:
-            for i, char in enumerate(word):
-                chars[(i, char)] += 1
+        for i in range(len(words[0])):
+            for j in range(len(words)):
+                chars[i][words[j][i]] += 1
                 
         @cache
         def dp(i, j):
@@ -15,7 +15,8 @@ class Solution:
             if i >= len(words[0]):
                 return 0
           
-            count = dp(i + 1, j) + chars[(i,target[j])] * dp(i + 1,j + 1)
-            return count % mod
+            count = (dp(i + 1, j) + (chars[i][target[j]] * dp(i + 1, j + 1))) % mod
+
+            return count
                 
         return dp(0, 0) % mod
