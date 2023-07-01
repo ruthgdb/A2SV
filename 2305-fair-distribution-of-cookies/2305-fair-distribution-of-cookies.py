5c26aaad44a0f77ae497c895d6dc9e22bc5ec9a5
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-07-01 11:15:18 +0300

    Time: 3560 ms (24.26%), Space: 16.5 MB (21.27%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -3,21 +3,20 @@ class Solution:
         children = [0] * k
         minDiff = float("inf")
         
-        @cache
-        def backtrack(i, currMax):
+        def backtrack(i):
             nonlocal minDiff
             
             if i == len(cookies):
-                minDiff = min(minDiff, currMax)
+                minDiff = min(minDiff, max(children))
                 return 
             
             for j in range(k):
                 children[j] += cookies[i]
                 if children[j] < minDiff:
-                    backtrack(i + 1, max(currMax, children[j]))
+                    backtrack(i + 1)
                 children[j] -= cookies[i]
                 
-            return currMax
+            return
         
-        backtrack(0, 0)
+        backtrack(0)
         return minDiff
