a9701fc0667370ed9c3bb7a12b60a62e0a51b2d8
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-07-05 19:01:45 +0300

    Time: 2032 ms (50.30%), Space: 35 MB (37.23%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -5,9 +5,11 @@ class NumArray:
         self.length = 2 ** (math.ceil(math.log2(len(nums))))
         self.arr = [0] * (2 * self.length - 1)
         self.nums = nums
+        
         # insert the numbers in the tree
         for i, num in enumerate(nums):
             self.arr[self.length - 1 + i] = num
+            
         #bottom up
         for i in range(self.length - 2, -1, -1):
             self.arr[i] = self.arr[2 * i + 1] + self.arr[2 * i + 2]
@@ -16,9 +18,11 @@ class NumArray:
         diff = val - self.nums[index]
         self.nums[index] = val
         start = self.length - 1 + index
+        
         while start != 0:
             self.arr[start] += diff
             start = (start - 1) // 2
+            
         self.arr[0] += diff
     
     def rangeSumFromZero(self, until):
@@ -26,6 +30,7 @@ class NumArray:
         curr_ind = 0
         start = 0
         end = self.length - 1
+        
         while curr_ind < self.length - 1:
             if (end + start) // 2 < until:
                 total += self.arr[2 * curr_ind + 1]
@@ -34,12 +39,13 @@ class NumArray:
             else:
                 curr_ind = 2 * curr_ind + 1
                 end = (end + start) // 2
+                
         return total + self.arr[curr_ind]
                 
-        
     def sumRange(self, left: int, right: int) -> int:
         if left == 0:
             return self.rangeSumFromZero(right)
+        
         return self.rangeSumFromZero(right) - self.rangeSumFromZero(left - 1)
 
 
