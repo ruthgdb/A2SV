6d28b7996c8a8e34816c914df28c73a8312f25a0
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-07-15 12:49:10 +0300

    Time: 864 ms (91.11%), Space: 210.5 MB (7.41%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -8,14 +8,10 @@ class Solution:
             if i == len(events) or count == k:
                 return 0
             
-            cont = 0
-            
-            if count < k:
-                idx = bisect.bisect_right(starts, events[i][1])
-                cont = events[i][2] + dp(idx, count + 1)
-                
-            start = dp(i + 1, count)
-            return max(start, cont)
+            idx = bisect.bisect_right(starts, events[i][1])
+            take = events[i][2] + dp(idx, count + 1)
+            skip = dp(i + 1, count)
+            return max(take, skip)
         
         return dp(0, 0)
         
