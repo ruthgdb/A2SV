d3eb58be118364ad4445d4d0b6b75c5c70cc4c08
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-08-24 11:30:51 +0300

    Time: 40 ms (72.19%), Space: 16.5 MB (31.04%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,24 +1,56 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        fullText = []
-        line = []
-        currLen = 0
+        full_text = []
+        curr_line = []
+        word_count = 0
+        
+        def build_line(line, count):
+            remaining_space = maxWidth - count
+            text = []
+            places = len(line) - 1
+            
+            if places == 0:
+                return ''.join(line[0] + ' ' * remaining_space)
+            
+            extras = remaining_space % places
+            spaces = remaining_space // places
+            
+            for i in line:
+                text.append(i)
+                if extras > 0:
+                    space = ' ' * (spaces + 1)
+                    extras -= 1
+                else:
+                    space = ' ' * spaces
+                text.append(space)
+            
+            text.pop()
+            return ''.join(text)
         
         for word in words:
-            if len(line) + len(word) > maxWidth - currLen:
-                for i in range(maxWidth - currLen):
-                    idx = i % max(len(line) - 1, 1)
-                    line[idx] += ' '
-                    
-                fullText.append("".join(line))
-                line = []
-                currLen = 0
+            if len(word) + max(0, len(curr_line)) + word_count <= maxWidth:
+                curr_line.append(word)
+                word_count += len(word)
+            else:
+                text = build_line(curr_line, word_count)
+                full_text.append(text)
+                curr_line = [word]
+                word_count = len(word)
                 
-            currLen += len(word)
-            line.append(word)
+        if curr_line:
+            text = []
+            remaining_space = maxWidth - word_count
             
-        line = " ".join(line)
-        line = line + ' ' * (maxWidth - len(line))
-        fullText.append("".join(line))
+            for i in curr_line:
+                text.append(i)
+                
+                if remaining_space > 0:
+                    text.append(' ')
+                    remaining_space -= 1
+               
+            if remaining_space > 0:
+                text.append(' ' * remaining_space)
+                
+            full_text.append(''.join(text))
         
-        return fullText
+        return full_text
