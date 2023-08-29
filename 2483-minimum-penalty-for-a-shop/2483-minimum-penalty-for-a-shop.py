53ff447203ab62f40601e661498e4c0426d27553
Ruth Getachew <42246536+ruthgdb@users.noreply.github.com>
2023-08-29 11:35:14 +0300

    Time: 226 ms (32.23%), Space: 21.3 MB (30.42%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,27 +1,20 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        pref_sum = [int(customers[0] == 'N')]
         post_sum = [int(customers[-1] == 'Y')]
         
-        for i in range(1, len(customers)):
-            pref_sum.append(pref_sum[-1] + int(customers[i] == 'N'))
-            
         for i in range(len(customers) - 2, -1, -1):
             post_sum.append(post_sum[-1] + int(customers[i] == 'Y'))
           
         post_sum.reverse()
+        n_count = 0
         min_cost = post_sum[0]
         min_hour = 0
         
         for i in range(len(customers)):
-            before = pref_sum[i - 1] if i > 0 else 0
-            after = post_sum[i]
-            
-            if before + after < min_cost:
-                min_cost = before + after
+            if n_count + post_sum[i] < min_cost:
+                min_cost = n_count + post_sum[i]
                 min_hour = i
                 
-        if pref_sum[-1] < min_cost:
-            min_hour = len(customers)
-            
-        return min_hour
+            n_count += customers[i] == 'N'
+                
+        return min_hour if n_count >= min_cost else len(customers)
